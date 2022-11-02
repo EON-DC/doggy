b1e100673ce5d5519ce1df7a6c0244a45a9e8dd4
EON-DC <mun0392@gmail.com>
2022-11-02 19:40:59 +0900

    Initial commit

---
+++ src/main/java/com/doggy/EonApplication.java
@@ -0,0 +1,13 @@
+package com.doggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EonApplication.class, args);
+	}
+
+}
