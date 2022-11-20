7d644d9555f9f9e871400bd0e22e28270fe434e3
EON-DC <mun0392@gmail.com>
2022-11-20 14:39:41 +0900

    free coding

--- src/main/java/com/doggy/EonApplication.java
+++ src/main/java/com/doggy/EonApplication.java
@@ -1,14 +1,7 @@
 package com.doggy;
 
-import com.doggy.model.Post;
-import com.doggy.model.User;
-import com.doggy.repository.PostRepository;
-import com.doggy.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class EonApplication {
@@ -17,14 +10,5 @@ public class EonApplication {
         SpringApplication.run(EonApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(PostRepository posts, UserRepository users, PasswordEncoder encoder) {
-        return args -> {
-            users.save(new User("park", encoder.encode("password"), "ROLE_USER"));
-            users.save(new User("admin", encoder.encode("password"), "ROLE_ADMIN"));
-            posts.save(new Post("hello, world!", "hello-word", "Welcome to my blog!", "park"));
-
-        };
-    }
 
 }
--- src/main/java/com/doggy/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.doggy.repository;
-
-import com.doggy.model.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-
-}
