7ca7e0184562e7de60d5686f874d6b8c02862530
EON-DC <mun0392@gmail.com>
2022-11-28 08:25:10 +0900

    free coding

--- src/test/java/com/doggy/ControllerTest.java
+++ src/test/java/com/doggy/ControllerTest.java
@@ -3,29 +3,49 @@ package com.doggy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ProductController.class)
 public class ControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private ProductService productService;
 
     @Test
-    @DisplayName("")
+    @DisplayName("다 가져올 수 있는지?")
     void tdd_for_getProduct() throws Exception {
+
+
+        Product product = new Product(1L, "CD", 3, 20000);
+
         // given
-        BDDMockito.given(productService.saveProduct(new Product())).willReturn(null);
+        given(productService.findByIdProduct(1L))
+                .willReturn(product);
 
-
+        mockMvc.perform(get("/api/v1/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").exists())
+                .andExpect(jsonPath("$.stock").exists())
+                .andExpect(jsonPath("$.price").exists())
+                .andDo(print());
+        verify(productService).findByIdProduct(1L);
     }
 
 }
